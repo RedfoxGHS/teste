b3d878010918c3a3d9a262e94b7abf267bb8c18b
GabrielHS <gabrielhenri0012@gmail.com>
2022-09-01 15:54:10 -0300

    add test

---
+++ src/test/java/com/example/demo/service/impl/GeneralTaskServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class GeneralTaskServiceImplTest {
+
+    @Test
+    public void testGithubActionsSuccess(){
+        assertEquals(10, 10);
+    }
+
+    @Test
+    public void testGithubActionsFail(){
+        assertEquals(10, 1);
+    }
+}
