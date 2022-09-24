506e5b285e4fad8412f523f20b436488c9a7acd5
GabrielHS <gabrielhenri0012@gmail.com>
2022-09-24 20:49:36 -0300

    teste email

--- scripts/send-email.sh
+++ scripts/send-email.sh
@@ -1,5 +1,4 @@
-ls
 echo "Inicio da instalação"
 sudo apt-get install mailutils
 echo "Fim da instalação"
-echo "ola mundo" | mail -s "subject:Exercicio_S207" gabrielhenri0012@gmail.com
+echo "ola mundo" | mail -s "subject: Exercicio_S207" gabrielhenri0012@gmail.com
